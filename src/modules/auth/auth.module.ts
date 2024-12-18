37f9062b98a47f9e246ff408299fc08d12e492a7
lucasmelojs <141846315+lucasmelojs@users.noreply.github.com>
2024-12-18 10:40:18 -0300

    fix: enhance auth module and add custom decorators

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { User } from '../../entities/user.entity';
@@ -10,19 +11,25 @@ import { UserModule } from '../user/user.module';
 
 @Module({
   imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
     TypeOrmModule.forFeature([User]),
     JwtModule.registerAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
-        secret: configService.get('JWT_SECRET'),
-        signOptions: { expiresIn: '15m' },
+        secret: configService.get('jwt.secret'),
+        signOptions: { 
+          expiresIn: configService.get('jwt.accessTokenExpiration'),
+        },
       }),
       inject: [ConfigService],
     }),
     UserModule,
   ],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy],
-  exports: [AuthService],
+  providers: [
+    AuthService,
+    JwtStrategy,
+  ],
+  exports: [AuthService, JwtStrategy, PassportModule],
 })
 export class AuthModule {}
